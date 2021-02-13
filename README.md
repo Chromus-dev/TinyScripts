1f44983f9a88c434e5d55e49ad6dae9988b0fa20
Chromus_ <Chromus-dev@users.noreply.github.com>
2021-02-12 16:18:22 -0800

    updat

---
+++ README.md
@@ -0,0 +1,7 @@
+# TinyScripts
+<br>
+
+### A set of small scripts
+
+[![npm](https://img.shields.io/npm/v/tinyscripts)](https://www.npmjs.com/package/tinyscripts)
+[![npm size](https://img.shields.io/bundlephobia/min/tinyscripts)](https://www.npmjs.com/package/tinyscripts)
---
+++ index.js
@@ -0,0 +1,37 @@
+exports.base64 = function(type, text) {
+	if (!(type == 'encode' || type == 'decode')) throw new TypeError('You must specify encode or decode');
+	if (!type) throw new TypeError(`You must specify text to ${type}`);
+
+	function encode(text) {
+		let buff = new Buffer.from(text);
+		let base64data = buff.toString('base64');
+		let res = {
+			originalData: text,
+			buff: buff,
+			base64: base64data
+		};
+		return res;
+	}
+	function decode(text) {
+		let buff = new Buffer.from(text, 'base64');
+		let base64data = buff.toString('ascii');
+		let res = {
+			originalData: text,
+			buff: buff,
+			base64: base64data
+		};
+		return res;
+	}
+
+	try {
+		if (type == 'encode') {
+			let res = encode(text);
+			return res.base64;
+		} else {
+			let res = decode(text);
+			return res.base64;
+		}
+	} catch (err) {
+		console.error(err);
+	}
+};
